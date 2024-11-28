33a3b091af8826d9f6bad80e1bf2b591575ed103
Joker-04 <155981494+Joker-04@users.noreply.github.com>
2024-11-28 23:08:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 PICS = (environ.get('PICS', 'https://envs.sh/Yd8.jpg ,https://envs.sh/YdJ.jpg')).split() #SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://envs.sh/Yd8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
